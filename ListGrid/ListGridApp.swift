3cf3b19d2f5ca93e12ff37dded1db43a8ebc315c
KevinLopezAvila <keflopavi2001@gmail.com>
2024-07-22 09:43:09 -0600

    Initial Commit

---
+++ ListGrid/ListGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListGridApp.swift
+//  ListGrid
+//
+//  Created by KevinLA on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ListGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
